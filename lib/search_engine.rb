35bcb61086fb5dd4494982da771e1920867994f3
matle <matle@ucdavis.edu>
2016-03-02 21:58:29 +0000

    Trying to get URLs

--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -1,13 +1,23 @@
+require 'google-search'
 
 class SearchEngine
+  def initialize
+    @mySearch = Google::Search::Web.new
+  end
 
-	def initialize
-
-	end
-
-	def search(query)
-		r
-	end
+  def search(query)
+    @mySearch = Google::Search::Web.new do |searcher|
+    searcher.query = query
+    searcher.each_response { print '.'; $stdout.flush } # or STDOUT.sync = true
+    end
+    
+    return @mySearch.get_uri # stored as JSON, how to get URLS?
+  end
 
 end
 
+
+query = "C++ how to initialize array"
+search_engine = SearchEngine.new()
+results = search_engine.search(query)
+puts results
