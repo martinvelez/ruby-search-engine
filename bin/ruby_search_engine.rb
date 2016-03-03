903c2c039f7cb2f6e2f9031a22b3c11fdc768c1f
Martin Velez <mvelez999@gmail.com>
2016-03-02 17:30:21 -0800

    working with Mat

--- bin/ruby_search_engine.rb
+++ bin/ruby_search_engine.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'ruby-search-engine'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib','search_engine.rb'))
 
 
 # TODO: Make this into a better command-line app
 
 query = ARGF.read
 search_engine = SearchEngine.new()
-results = search_egine.search(query)
-puts results
+results = search_engine.search(query)
+results.each do |r|
+	puts "#{r.index}\t#{r.title}\t#{r.uri}"
+end
--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -1,12 +1,21 @@
+require 'google-search'
 
 class SearchEngine
 
 	def initialize
-
+	
 	end
 
 	def search(query)
-		r
+		results = []
+		
+		# TODO: Replace with our own code
+		Google::Search::Web.new do |search|
+			search.query = query
+			search.size = :large
+		end.each { |item| results << item }
+
+		return results
 	end
 
 end
