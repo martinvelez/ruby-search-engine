04c9aa8eaa0b1b78739482a7228e6efcdf54669f
Martin Velez <mvelez999@gmail.com>
2016-03-02 18:00:43 -0800

    added base uri

--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -1,21 +1,33 @@
 require 'google-search'
+require 
 
 class SearchEngine
 
+	attr_accessor :uri
+
 	def initialize
-	
+		self.uri = 'http://www.google.com/uds/GwebSearch?start=0&rsz=large&hl=en&key=notsupplied&v=1.0&q='open+file+in+ruby%0A&filter=1
 	end
 
-	def search(query)
+	def search(query, num_of_pages=1)
 		results = []
 		
 		# TODO: Replace with our own code
-		Google::Search::Web.new do |search|
+		se = Google::Search::Web.new do |search|
 			search.query = query
 			search.size = :large
-		end.each { |item| results << item }
+		end
+		puts se.inspect
+		puts se.get_uri
+		se.each { |item| results << item }
 
 		return results
+	end
+
+	def get_uri(query)
+		query.strip!
+		# TODO url encode query
+		self.uri + query + '&filter=1'	
 	end
 
 end
